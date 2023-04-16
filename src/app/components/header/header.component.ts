c06c5bb86262bedd79bbab4cc8d569268f12e244
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-04-16 12:57:28 -0500

    learned how to make a toggle function to hide and show a section

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
-  title: string = 'Task Tracker'
+  title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription!: Subscription;
 
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+  }
 
   toggleAddTask() {
-    console.log("Toggle")
+    this.uiService.toggleAddTask();
   }
 }
